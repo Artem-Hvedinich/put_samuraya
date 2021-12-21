883cd23543e4a3421ed6c62b26873dc5306fa6ed
unknown <artem33771@gmail.com>
2021-12-22 01:54:02 +0300

    'lesson12'

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 const App = () => {
-  return (
-      <div>
-    <div> className="App"></div>
-
-      </div>
-  );
-}
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <Profile/>
+        </div>
+    );
+};
 
 export default App;
