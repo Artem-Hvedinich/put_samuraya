7ded756f1aa85dd13d90ade2ad8229c329346fe1
unknown <artem33771@gmail.com>
2021-12-23 21:01:08 +0300

    'lesson'

---
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import s from './MyPosts.module.css'
+
+const MyPosts = () => {
+    return (
+        <div>
+            <div>
+                My post
+            </div>
+            <div>
+                New post
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    )
+}
+
+export default MyPosts;
